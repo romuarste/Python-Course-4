b68c2312dbbd7d02893463de9c45b5f6b0b26687
Roman Stelmakh <romuarste@gmail.com>
2023-07-31 08:52:54 +1100

    End of lesson 7

--- advertisements/app/models.py
+++ advertisements/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    auction = models.BooleanField(default=False, verbose_name='Торг')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
