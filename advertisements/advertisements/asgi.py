434787fd763f1c84e3b5a55e441766487b791d2d
Roman Stelmakh <romuarste@gmail.com>
2023-07-30 22:14:58 +1100

    fixed spelling error in app name

--- advertisements/advertisements/asgi.py
+++ advertisements/advertisements/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for adverisements project.
+ASGI config for advertisements project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adverisements.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advertisements.settings')
 
 application = get_asgi_application()
--- advertisements/advertisements/wsgi.py
+++ advertisements/advertisements/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for adverisements project.
+WSGI config for advertisements project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adverisements.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advertisements.settings')
 
 application = get_wsgi_application()
