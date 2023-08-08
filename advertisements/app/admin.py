253f7149d9175790b6a10f3e0c6bcb49736e0e1e
Roman Stelmakh <romuarste@gmail.com>
2023-08-08 19:28:09 +1100

    Lesson 8

--- advertisements/app/admin.py
+++ advertisements/app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Advertisement
 
 # Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', Advertisement.created_date, 'auction']
+    list_filter = ['auction', 'created_at']
+    actions = ['make_auction_as_false']
+    fieldsets = [
+        ('Общее', {
+            'fields': ('title', 'description')
+        }),
+        ('Финансы', {
+            'fields': ('price', 'auction')
+        })
+    ]
+
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+admin.site.register(Advertisement, AdvertisementAdmin)
--- advertisements/app/models.py
+++ advertisements/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 class Advertisement(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название')
@@ -8,8 +9,18 @@ class Advertisement(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
 
-    class Meta:
-        db_table = 'advertisements'
+    @admin.display(description='дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
 
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span>Сегодня в {}</span>',created_time)
+        return self.created_at.strftime("%d.%m.%y в %H:%M:%S")
+    
     def __str__(self):
         return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
+    
+    class Meta:
+        db_table = 'advertisements'
